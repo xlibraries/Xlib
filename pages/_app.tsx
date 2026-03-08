d8f82b1bfd8abf4e513a5afd76ccb63b4360f4f8
Xlib <aman9893089064@gmail.com>
2026-03-08 16:19:52 +0530

    aded analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 // @ts-nocheck
 import 'tailwindcss/tailwind.css'
 import '../styles/index.css'
+import { Analytics } from "@vercel/analytics/next"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
